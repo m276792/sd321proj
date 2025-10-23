3a2a4c5d90c95cafb82b09ae080651e78656d3b7
LUCY WEEMS <m276792@usna.edu>
2025-10-23 08:37:34 -0400

    world bank

---
+++ worldbankdf.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv('world_bank_data_2025.csv')
+
+
+df = df[['country_name','year','Inflation (CPI %)','GDP (Current USD)','GDP per Capita (Current USD)','Unemployment Rate (%)','GDP Growth (% Annual)','Current Account Balance (% GDP)','Government Expense (% of GDP)','Government Revenue (% of GDP)','Gross National Income (USD)']]
+df = df.dropna()
+print(df.head())
