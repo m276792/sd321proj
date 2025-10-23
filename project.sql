c8621047a9653c68052cea448cdef0663366ec61
LUCY WEEMS <m276792@usna.edu>
2025-10-23 09:09:52 -0400

    m

--- project.sql
+++ project.sql
@@ -1,6 +1,6 @@
 
 
-print('world_bank table')
+SELECT('world_bank table');
 
 DROP TABLE IF EXISTS `WorldBank`;
 
@@ -13,4 +13,17 @@ CREATE TABLE `WorldBank` (
   Unemployment VARCHAR(50) NOT NULL,
   GDPgrowth VARCHAR(50)
   
+);
+
+SELECT('emissions table');
+
+DROP TABLE IF EXISTS 'Emissions';
+
+CREATE TABLE `Emissions` (
+    country VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    total VARCHAR(50) NOT NULL,
+    coal VARCHAR(50) NOT NULL,
+    oil VARCHAR(50) NOT NULL,
+    gas VARCHAR(50) NOT NULL
 );
