67136766a9ca7ddf49cebea64dd5c2f3916da9df
m271974 <m271974@usna.edu>
2025-10-23 09:08:21 -0400

    adding

--- events.py
+++ events.py
@@ -1,13 +1,22 @@
 #cleaning global_climate_events_economic_impact_2020_2025.csv
 
 import pandas as pd
-
 df = pd.read_csv('global_climate_events_economic_impact_2020_2025.csv')
-#specifiy columns - keeps countries where there is less available data but need/want total, coal, oil, gas for each country at least
-
 df = df[['year', 'country','event_type','economic_impact_million_usd', 'international_aid_million_usd','impact_per_capita']]
 #print(df)
+df = df.dropna(how='any', axis=0, subset=['year', 'country','event_type','economic_impact_million_usd', 'international_aid_million_usd','impact_per_capita'])
+#print(df)
 
-clean = df.dropna(how='any', axis=0, subset=['year', 'country','event_type','economic_impact_million_usd', 'international_aid_million_usd','impact_per_capita'])
 
-print(clean)
+
+def events_impact(df, events_impact):
+    mapping = {
+        'year': 'INT', 
+        'country': 'VARCHAR(50)',
+        'event_type': 'VARCHAR(50)',
+        'economic_impact_million_usd': 'INT',
+        'international_aid_million_usd': 'INT',
+        'impact_per_capita': 'INT'
+    }
+
+create = f"CREATE TAVLE {events_impact} (\n"
---
+++ events.sql
@@ -0,0 +1,8 @@
+CREATE TABLE events_impact( 
+    year INT, 
+    country VARCHAR(50),
+    event_type VARCHAR(50),,
+    economic_impact_million_usd INT,
+    international_aid_million_usd INT,
+    impact_per_capita INT
+);k
