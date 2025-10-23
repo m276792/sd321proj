76e0803fe09d5c9b68f359e964ba881f00fd0ff9
m271974 <m271974@usna.edu>
2025-10-23 08:39:18 -0400

    eventsclean

---
+++ events.py
@@ -0,0 +1,13 @@
+#cleaning emissions.csv
+
+import pandas as pd
+
+df = pd.read_csv('global_climate_events_economic_impact_2020_2025.csv')
+#specifiy columns - keeps countries where there is less available data but need/want total, coal, oil, gas for each country at least
+
+df = df[['year', 'country','event_type','economic_impact_million_usd', 'international_aid_million_usd','impact_per_capita']]
+#print(df)
+
+clean = df.dropna(how='any', axis=0, subset=['year', 'country','event_type','economic_impact_million_usd', 'international_aid_million_usd','impact_per_capita'])
+
+print(clean)
