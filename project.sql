74b6fd53b169225205ab186be63759566b0ead59
m271974 <m271974@usna.edu>
2025-10-29 19:39:34 -0400

    update

--- project.sql
+++ project.sql
@@ -27,3 +27,13 @@ CREATE TABLE `Emissions` (
     oil VARCHAR(50) NOT NULL,
     gas VARCHAR(50) NOT NULL
 );
+
+
+CREATE TABLE events_impact( 
+    year INT, 
+    country VARCHAR(50),
+    event_type VARCHAR(50),,
+    economic_impact_million_usd INT,
+    international_aid_million_usd INT,
+    impact_per_capita INT
+);
