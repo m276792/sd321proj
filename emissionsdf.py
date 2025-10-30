46d536f0d9c5fb6f46a2031a6cccc18d722ecadd
m271974 <m271974@usna.edu>
2025-10-29 20:27:01 -0400

    m

--- emissionsdf.py
+++ emissionsdf.py
@@ -1,17 +1,16 @@
 #cleaning emissions.csv
 
 import pandas as pd
+def clean_emissions():
+    df = pd.read_csv('emissions.csv')
+    #specifiy columns - keeps countries where there is less available data but need/want total, coal, oil, gas for each country at least
 
-df = pd.read_csv('emissions.csv')
-#specifiy columns - keeps countries where there is less available data but need/want total, coal, oil, gas for each country at least
+    df = df.dropna(how='any', axis=0, subset=['Country','Total', 'Coal', 'Oil', 'Gas'])
 
-df = df.dropna(how='any', axis=0, subset=['Country','Total', 'Coal', 'Oil', 'Gas'])
+    #returns first lines of df
+    #print(df.head())
+    #note, still has NaN values
 
-#returns first lines of df
-#print(df.head())
-#note, still has NaN values
-
-df = df[['Country','Year','Total', 'Coal', 'Oil', 'Gas']]
-print(df.head())
-
+    df = df[['Country','Year','Total', 'Coal', 'Oil', 'Gas']]
+    return df
 
--- teamX_load.py
+++ teamX_load.py
@@ -1,7 +1,11 @@
 from events import clean_events
+from emissionsdf import clean_emissions
 from MySQLdb import dbConnect
 
 db = dbConnect()
 
 events_df = clean_events()
 events_df.to.sql('Events', db, if_exists='replace', index=False)
+
+emissions_df = clean_emissions()
+emissions_df.to.sql('Emissions', db, if_exists='replace', index=False)
