c13c150a46203d839d156469b289ef09d99e5bf3
m271974 <m271974@usna.edu>
2025-10-30 08:42:36 -0400

    m

--- project.sql
+++ project.sql
@@ -1,62 +1,61 @@
-
-DROP TABLE IF EXISTS Emissions;
-DROP TABLE IF EXISTS Events_Impact;
-DROP TABLE IF EXISTS WorldBank;
-
-SELECT('WorldBank Tabl');
-CREATE TABLE WorldBank(
-  country_name VARCHAR(50) NOT NULL,
-  year INT NOT NULL,
-  Inflation VARCHAR(50) NOT NULL,
-  GDP INT NOT NULL,
-  GDPpercapita INT NOT NULL,
-  Unemployment VARCHAR(50) NOT NULL,
-  GDPgrowth VARCHAR(50) NOT NULL
+DROP TABLE IF EXISTS WorldBank ;
+CREATE TABLE WorldBank (
+  country_name                      VARCHAR(20)   NOT NULL,
+  country_id                        VARCHAR(60)   NULL,
+  year                              INT           NULL,
+  Inflation_(CPI_%)                 VARCHAR(64)   NULL,
+  GDP_(Current_USD)                 VARCHAR(64)   NULL,
+  GDP_per_Capita_(Current_USD)      VARCHAR(64)   NULL,
+  Unemployment_Rate_(%)             VARCHAR(64)   NULL,
+  "Interest_Rate_(Real              VARCHAR(64)   NULL,
+  _%)"                              VARCHAR(64)   NULL,
+  "Inflation_(GDP_Deflator          VARCHAR(64)   NULL,
+  _%)"                              VARCHAR(64)   NULL,
+  GDP_Growth_(%_Annual)             VARCHAR(64)   NULL,
+  Current_Account_Balance_(%_GDP)   VARCHAR(64)   NULL,
+  Government_Expense_(%_of_GDP)     VARCHAR(64)   NULL,
+  Government_Revenue_(%_of_GDP)     VARCHAR(64)   NULL,
+  Tax_Revenue_(%_of_GDP)            VARCHAR(64)   NULL,
+  Gross_National_Income_(USD)       VARCHAR(64)   NULL,
+  Public_Debt_(%_of_GDP)            VARCHAR(64)   NULL
 );
 
-SELECT('Emissions Table');
-CREATE TABLE Emissions(
-    country VARCHAR(50) NOT NULL,
-    year INT NOT NULL,
-    total VARCHAR(50) NOT NULL,
-    coal VARCHAR(50) NOT NULL,
-    oil VARCHAR(50) NOT NULL,
-    gas VARCHAR(50) NOT NULL
+DROP TABLE IF EXISTS Events ;
+CREATE TABLE Events (
+  event_id                        VARCHAR(20)   NOT NULL,
+  date                            VARCHAR(60)   NULL,
+  year                            INT           NULL,
+  month                           VARCHAR(64)   NULL,
+  country                         VARCHAR(64)   NULL,
+  event_type                      VARCHAR(64)   NULL,
+  severity                        VARCHAR(64)   NULL,
+  duration_days                   VARCHAR(64)   NULL,
+  affected_population             VARCHAR(64)   NULL,
+  deaths                          VARCHAR(64)   NULL,
+  injuries                        VARCHAR(64)   NULL,
+  economic_impact_million_usd     VARCHAR(64)   NULL,
+  infrastructure_damage_score     VARCHAR(64)   NULL,
+  response_time_hours             VARCHAR(64)   NULL,
+  international_aid_million_usd   VARCHAR(64)   NULL,
+  latitude                        VARCHAR(64)   NULL,
+  longitude                       VARCHAR(64)   NULL,
+  total_casualties                VARCHAR(64)   NULL,
+  impact_per_capita               VARCHAR(64)   NULL,
+  aid_percentage                  VARCHAR(64)   NULL
 );
 
-SELECT('Events Table');
-CREATE TABLE Events( 
-    year INT NOT NULL, 
-    country VARCHAR(50) NOT NULL,
-    event_type VARCHAR(50) NOT NULL,
-    economic_impact_million_usd INT NOT NULL,
-    international_aid_million_usd INT NOT NULL,
-    impact_per_capita INT NOT NULL
+DROP TABLE IF EXISTS Emissions ;
+CREATE TABLE Emissions (
+  "Country"              VARCHAR(20)   NOT NULL,
+  "ISO_3166_1_alpha_3"   VARCHAR(60)   NULL,
+  "Year"                 INT           NULL,
+  "Total"                VARCHAR(64)   NULL,
+  "Coal"                 VARCHAR(64)   NULL,
+  "Oil"                  VARCHAR(64)   NULL,
+  "Gas"                  VARCHAR(64)   NULL,
+  "Cement"               VARCHAR(64)   NULL,
+  "Flaring"              VARCHAR(64)   NULL,
+  "Other"                VARCHAR(64)   NULL,
+  "Per_Capita"           VARCHAR(64)   NULL
 );
 
-
-LOAD DATA INFILE 'global_climate_events_economic_impact_2020_2025.csv'
-INTO TABLE Events
-FIELDS TERMINATED BY ',' /*column seperator*/
-ENCLOSED BY '"' /*if fields have quotes*/
-LINES TERMINATED BY '\n' /*line seperator*/
-IGNORE 1 ROWS /*skip header*/
-(year, country, event_type, economic_impact_million_usd, international_aid_million_usd, impact_per_capita);
-
-
-LOAD DATA INFILE 'emissions.csv'
-INTO TABLE Emissions
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(country, year, total, coal, oil, gas);
-
-
-LOAD DATA INFILE 'world_bank_data_2025.csv'
-INTO TABLE WorldBank
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(country_name, year, Inflation, GDP, GDPpercapita, Unemployment, GDPgrowth);
