7f40eacf3a7108d5d86e956fedf2a259dfae2293
m271974 <m271974@usna.edu>
2025-10-29 20:29:15 -0400

    m

--- teamX_load.py
+++ teamX_load.py
@@ -1,5 +1,6 @@
 from events import clean_events
 from emissionsdf import clean_emissions
+from worldbankdf import clean_worldbank
 from MySQLdb import dbConnect
 
 db = dbConnect()
@@ -9,3 +10,7 @@ events_df.to.sql('Events', db, if_exists='replace', index=False)
 
 emissions_df = clean_emissions()
 emissions_df.to.sql('Emissions', db, if_exists='replace', index=False)
+
+
+worldbank_df = clean_worldbank()
+worldbank_df.to.sql('WorldBank', db, if_exists='replace', index=False)
--- worldbankdf.py
+++ worldbankdf.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('world_bank_data_2025.csv')
+def clean_worldbank():
+    df = pd.read_csv('world_bank_data_2025.csv')
 
-#might take more columns out
-df = df[['country_name','year','Inflation (CPI %)','GDP (Current USD)','GDP per Capita (Current USD)','Unemployment Rate (%)','GDP Growth (% Annual)']]
-df = df.dropna()
-print(df.tail())
-
-print(df.info())
+    #might take more columns out
+    df = df[['country_name','year','Inflation (CPI %)','GDP (Current USD)','GDP per Capita (Current USD)','Unemployment Rate (%)','GDP Growth (% Annual)']]
+    df = df.dropna()
+    return df
