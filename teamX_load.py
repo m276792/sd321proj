2cc1c5c3ad58d5beca072d6cf6d073bd19fe755b
m271974 <m271974@usna.edu>
2025-10-29 20:30:44 -0400

    m

--- teamX_load.py
+++ teamX_load.py
@@ -6,11 +6,11 @@ from MySQLdb import dbConnect
 db = dbConnect()
 
 events_df = clean_events()
-events_df.to.sql('Events', db, if_exists='replace', index=False)
+events_df.to.sql('Events', con=db, if_exists='replace', index=False)
 
 emissions_df = clean_emissions()
-emissions_df.to.sql('Emissions', db, if_exists='replace', index=False)
+emissions_df.to.sql('Emissions', con=db, if_exists='replace', index=False)
 
 
 worldbank_df = clean_worldbank()
-worldbank_df.to.sql('WorldBank', db, if_exists='replace', index=False)
+worldbank_df.to.sql('WorldBank', con=db, if_exists='replace', index=False)
