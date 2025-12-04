cdb732e0a089e0ae8373f985aa3942c5dee30428
LUCY WEEMS <m276792@usna.edu>
2025-12-03 21:00:50 -0500

    m

--- team1_part5.R
+++ team1_part5.R
@@ -41,7 +41,7 @@ df_long <- df %>%
     values_to = "Value"
   )
 
-# Bar plot grouped by indicator
+# bar plot
 ggplot(df_long, aes(x = Year, y = Value, fill = Indicator)) +
   geom_col(position = "dodge") +
   facet_wrap(~ Country) +
@@ -53,7 +53,36 @@ ggplot(df_long, aes(x = Year, y = Value, fill = Indicator)) +
 ggsave("R_country_indicators_barplot.png", dpi = 300, width = 12, height = 6)
 
 
-#####Graph2
+db <- dbConnect(RMariaDB::MariaDB(),
+    user     = user,
+    password = password,
+    host     = host,
+    dbname   = database)
+
+query <- "
+SELECT Country, Year, 
+CAST(GDP AS DECIMAL(17,2)) AS GDP
+FROM WorldBank
+WHERE Year = '2019'
+AND GDP IS NOT NULL
+ORDER BY GDP DESC
+LIMIT 16;
+"
+
+df <- dbGetQuery(db, query)
+
+# pie chart
+ggplot(df, aes(x = "", y = GDP, fill = Country)) +
+  geom_col(width = 1) +
+  coord_polar(theta = "y") +
+  theme_void() +
+  ggtitle("GDP Distribution (2019)") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggsave("R_GDP_PieChart_2019.png", dpi = 300, width = 8, height = 8)
+
+
+
 db <- dbConnect(RMariaDB::MariaDB(),
     user     = user,
     password = password,
@@ -64,13 +93,10 @@ query <- "
 SELECT
     e.Country,
     AVG(CAST(e.Total AS DECIMAL(18,3))) AS avg_total_emissions,
-    AVG(CAST(e.Coal AS DECIMAL(18,3))) AS avg_coal_emissions,
-    AVG(CAST(e.Oil AS DECIMAL(18,3))) AS avg_oil_emissions,
-    AVG(CAST(e.Gas AS DECIMAL(18,3))) AS avg_gas_emissions,
     AVG(CAST(ev.EconomicImpact AS DECIMAL(18,2))) AS avg_impact,
     SUM(CAST(ev.EconomicImpact AS DECIMAL(18,2))) AS total_impact,
-    COUNT(ev.event_type) AS event_count
-
+    COUNT(ev.event_type) AS event_count,
+    SUM(total_impact) / SUM(avg_total_emissions) AS ratio
 FROM Emissions e
 LEFT JOIN Events ev
     ON e.Country = ev.Country
@@ -81,7 +107,7 @@ WHERE e.Year IN (2019, 2020, 2021)
   AND ev.event_type IN ('Drought','Heatwave','Wildfire','Cold Wave','Flood','Landslide')
 
 GROUP BY e.Country
-ORDER BY avg_total_emissions DESC
+ORDER BY event_count DESC
 LIMIT 16;
 "
 
@@ -90,7 +116,7 @@ df <- dbGetQuery(db, query)
 # Prepare data for plotting
 df_long <- df %>%
   pivot_longer(
-    cols = c(avg_total_emissions, avg_impact, total_impact),
+    cols = c(avg_total_emissions, ratio, total_impact),
     names_to = "Metric",
     values_to = "Value"
   )
@@ -105,7 +131,9 @@ ggplot(df_long, aes(x = Country, y = Value, fill = Metric)) +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggsave("R_graph1.png",
+ggsave("R_emissions_vs_impact_per_country.png",
        dpi = 300, width = 12, height = 6)
+
+
 
 
